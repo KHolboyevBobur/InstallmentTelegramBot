8bd2abd901f4a1c22413d1513597aaa7bf7774f6
Bobur <kholboyev_bobur@mail.ru>
2022-12-29 16:14:16 +0500

    added-attribute

--- InstallmentTelegramBot/Models/Base/BaseModel.cs
+++ InstallmentTelegramBot/Models/Base/BaseModel.cs
@@ -1,7 +1,15 @@
-﻿namespace InstallmentTelegramBot.Models.Base;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace InstallmentTelegramBot.Models.Base;
 
 public class BaseModel
 {
+
+    [JsonPropertyName("id")]
     public int Id { get; set; }
+
+    [Required]
+    [JsonPropertyName("name")]
     public string Name { get; set; }
 }
